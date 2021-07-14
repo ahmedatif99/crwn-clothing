9447684ac5fdf89a84a1486f5a6ad5b0fddce0f3
ahmedatif99 <ahmedahmed1919a@gmail.com>
2021-07-14 19:30:54 +0300

    separating our app into client folder, add express server to help handle backend stripe payment

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyPrarser = require('body-parser');
+const path = require('path');
+
+if (process.env.NOED_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.port || 5000;
+
+app.use(bodyPrarser.json());
+app.use(bodyPrarser.urlencoded({ extended: true }));
+
+
+if (process.env.NOED_ENV === 'porduction') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    });
+}
+
+app.listen(port, error => {
+    if (error) throw error;
+    console.log('server running on port ' + port);
+});
+
+app.post('/payment', (req, res) => {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    };
+
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if (stripeErr) {
+            res.status(500).send({ error: stripeErr });
+        } else {
+            res.status(200).send({ success: stripeRes });
+        }
+    });
+
+})
